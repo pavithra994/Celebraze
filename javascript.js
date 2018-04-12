d6c3e6620607197e211c24fc93c29e8eb80e001e
isangeeth95 <isangeeth95@gmail.com>
2018-04-13 03:00:44 +0530

    I added these files

---
+++ javascript.js
@@ -0,0 +1,44 @@
+function mySlide() {
+    var x = document.getElementsByClassName("slideShow");
+
+
+
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    myIndex++;
+    if (myIndex <= x.length) {
+        x[myIndex - 1].style.display = "block";
+    }
+
+    if (myIndex > x.length)
+    {
+        myIndex=1;
+        x[myIndex - 1].style.display = "block";
+    }
+
+        setTimeout(mySlide, 4000);
+
+}
+
+
+
+function navBarScrolled() {
+
+    var nv=document.getElementById("nav1");
+    //var value=nv.offsetTop;
+
+    if(pageYOffset>890)
+    {
+        nv.classList.add("sticky");
+    }
+
+    else{
+        nv.classList.remove("sticky");
+    }
+
+}
+
+
